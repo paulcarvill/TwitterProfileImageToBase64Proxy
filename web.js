40eedbab665cb3c227bf4b23b9969753867fe553
Paul Carvill <paul@paulcarvill.com>
2012-04-22 18:51:27 +0100

    initial commit

---
+++ web.js
@@ -0,0 +1,14 @@
+// https://devcenter.heroku.com/articles/nodejs
+
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  response.send('Hello paul!');
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
